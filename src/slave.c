ramonhpr/knot-modbus-source

commit 03ffbdd338937045f8b2edde0d440eb57b52a127

Add property 'Address' to slave
This patch adds 'Address' property to slaves object. Address includes
"hostname:port" of the peer/PLC.

src/slave.c
@@ -434,6 +434,22 @@ static struct l_dbus_message *property_set_name(struct l_dbus *dbus,
 	return NULL;
 }
 
+static bool property_get_address(struct l_dbus *dbus,
+				  struct l_dbus_message *msg,
+				  struct l_dbus_message_builder *builder,
+				  void *user_data)
+{
+	struct slave *slave = user_data;
+	char *address = l_strdup_printf("%s:%s", slave->hostname, slave->port);
+
+	/* PLC/Peer IP address */
+	l_dbus_message_builder_append_basic(builder, 's', &address);
+
+	l_free(address);
+
+	return true;
+}
+
 static bool property_get_enable(struct l_dbus *dbus,
 				  struct l_dbus_message *msg,
 				  struct l_dbus_message_builder *builder,
@@ -497,6 +513,12 @@ static void setup_interface(struct l_dbus_interface *interface)
 				       property_get_name,
 				       property_set_name))
 		l_error("Can't add 'Name' property");
+
+	/* Per/PLC IP address including port. Format: 'hostname:port' */
+	if (!l_dbus_interface_property(interface, "Address", 0, "s",
+				       property_get_address,
+				       NULL))
+		l_error("Can't add 'Address' property");
 
 	/* Enable/Disable slave polling */
 	if (!l_dbus_interface_property(interface, "Enable", 0, "b",

commit 18cc39bd92ed97a36d96fb505b90b2886387d6c3

Fix accessing invalid memory address
This patch fixes a bug introduced by 03ffbdd338937045f8b2edde0d440eb57b52a127.
String passed to message builder should not be released before
sending the message.

src/slave.c
@@ -42,6 +42,7 @@ struct slave {
 	bool enable;
 	char *name;
 	char *path;
+	char *ipaddress;
 	char *hostname;
 	char *port; /* getaddrinfo service */
 	modbus_t *tcp;
@@ -93,6 +94,7 @@ static void slave_free(struct slave *slave)
 	}
 	storage_close(slave->sources_fd);
 	l_free(slave->key);
+	l_free(slave->ipaddress);
 	l_free(slave->hostname);
 	l_free(slave->port);
 	l_free(slave->name);
@@ -441,12 +443,9 @@ static bool property_get_ipaddress(struct l_dbus *dbus,
 				  void *user_data)
 {
 	struct slave *slave = user_data;
-	char *address = l_strdup_printf("%s:%s", slave->hostname, slave->port);
 
 	/* PLC/Peer IP address */
-	l_dbus_message_builder_append_basic(builder, 's', &address);
-
-	l_free(address);
+	l_dbus_message_builder_append_basic(builder, 's', &slave->ipaddress);
 
 	return true;
 }